452e55f3b4f86e4662a731701784cc1786dab38c
Ali Zoljodi <ali.zoljodi@mdu.se>
2025-09-08 15:04:16 +0200

    baseline

--- brecq_base/deit_small_w4a4.sh
+++ brecq_base/deit_small_w4a4.sh
@@ -11,5 +11,5 @@
 
 source /home/alz07xz/project/PD-Quant/pd_quant/bin/activate
 echo "Starting Deit-Small W4A4 BRECQ experiment at $(date)"
-python ../run_script_seed.py --arch deit_small --w-bit 4 --a-bit 4 --seeds 1001 1002 1003 --sleep 0.5 --config ./configs/4bit/brecq_baseline.py --alpha-list 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --num-clusters-list 8 16 32 64 128 256 --pca-dim-list 1 25 50 75 100 125 150 175 200 220
+python ../run_script_seed.py --arch deit_small --w-bit 4 --a-bit 4 --seeds 1001 1002 1003 --sleep 0.5 --config ../configs/4bit/brecq_baseline.py --alpha-list 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --num-clusters-list 8 16 32 64 128 256 --pca-dim-list 1 25 50 75 100 125 150 175 200 220
 echo "Completed Deit-Small W4A4 BRECQ experiment at $(date)"
