f5852aee1185153eb4c3f25ec17d7185169c36c7
Ali Zoljodi <ali.zoljodi@mdu.se>
2025-09-14 14:27:20 +0200

    frg

---
+++ qdrop/vit_base_w6a6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J VIT_BASE_W6A6_QDROP
+#SBATCH -c 8
+#SBATCH --mem=128G
+#SBATCH -p gpu_computervision_long
+#SBATCH --gres=gpu:1
+#SBATCH --tmp=5G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<your-email-address>
+#SBATCH -t 4-00:00:00
+
+source /home/alz07xz/project/PD-Quant/pd_quant/bin/activate
+echo "Starting ViT-Base W6A6 QDROP experiment at $(date)"
+python ../run_script_seed.py --arch vit_base --w-bit 6 --a-bit 6 --seeds 1001 1002 1003 --sleep 0.5 --config ../configs/4bit/qdrop_baseline.py --alpha-list 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 --num-clusters-list 8 16 32 64 128 256 --pca-dim-list 1 25 50 75 100 125 150 175 200 220
+echo "Completed ViT-Base W6A6 QDROP experiment at $(date)"
